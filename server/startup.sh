55d766d0c7232cb4767a3889d98758a35dfab67c
albertomorini <albmor.dev@gmail.com>
2026-05-03 15:10:28 +0200

    startup script

--- server/startup.sh
+++ server/startup.sh
@@ -1,4 +1,23 @@
 
+##
+# root@GeoMedia-4GBUbuntu:~# sudo nano /etc/systemd/system/geomedia.service 
+# # [Unit]
+# # Description=GeoMedia Server backend on https port 9911
+# # After=network.target
+
+# # [Service]
+# # Type=simple
+# # ExecStart=/root/geomedia/server/startup.sh
+# # WorkingDirectory=/root/geomedia/server
+# # Restart=always
+# # User=root
+# # Environment=PYTHONUNBUFFERED=1
+
+# # [Install]
+# # WantedBy=multi-user.target
+
+
+
 # start docker container (mssql)
 docker start mssql
 
