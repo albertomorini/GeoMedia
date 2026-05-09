652e5d6f2c2a87557469c8d67576fc9444ce2ca4
albertomorini <albmor.dev@gmail.com>
2026-05-09 20:09:23 +0200

    both http and https, thus to allow debug

--- server/server.py
+++ server/server.py
@@ -345,15 +345,15 @@ async def handle_request(request: Request, full_path: str):
 # ------------------------------------------------
 # HTTP
 if __name__ == "__main__":
-    print(f"Server started on port: {PORT}")
-    uvicorn.run(app, host="0.0.0.0", port=PORT
+    print(f"Server started on port: {PORT-1}")
+    uvicorn.run(app, host="0.0.0.0", port=PORT-1
        
     )
-# ------------------------------------------------
+------------------------------------------------
 # HTTPS
-# if __name__ == "__main__":
-#     print(f"Server started on port: {PORT}")
-#     uvicorn.run(app, host="0.0.0.0", port=PORT,
-#         ssl_keyfile="/etc/letsencrypt/live/geomediasrv.duckdns.org/privkey.pem",
-#         ssl_certfile="/etc/letsencrypt/live/geomediasrv.duckdns.org/fullchain.pem",
-#     )
+if __name__ == "__main__":
+    print(f"Server started on port: {PORT}")
+    uvicorn.run(app, host="0.0.0.0", port=PORT,
+        ssl_keyfile="/etc/letsencrypt/live/geomediasrv.duckdns.org/privkey.pem",
+        ssl_certfile="/etc/letsencrypt/live/geomediasrv.duckdns.org/fullchain.pem",
+    )
